1620e246e5868dd78710966fc58530fbeff848c1
Paweł <pawel.rzanny@gmail.com>
2018-10-20 11:32:32 +0200

    [#2.1] [#2.2] Move your previous code to the CLI generated project

---
+++ 02-vue-cli-intro/my-app/src/components/DevMeeting.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="hello">
+    <ul>
+      <li
+        v-for="member in team"
+        :key="member">{{ member }}>
+        <button @click="removeTeamMember(member)">[x]</button>
+      </li>
+    </ul>
+    <input
+      v-model="newTeamMember"
+      type="text"
+      placeholder="New team member">
+    <button @click="addNewTeamMember()">Add hardcoded name!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      team: ['Aleksandra', 'Matusz', 'Maciej', 'Wojtek', 'Paweł'],
+      newTeamMember: undefined,
+    };
+  },
+  methods: {
+    addNewTeamMember() {
+      if (this.newTeamMember) {
+        this.team.push(this.newTeamMember);
+      }
+    },
+    removeTeamMember(memberToRemove) {
+      this.team.splice(this.team.indexOf(memberToRemove), 1);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
